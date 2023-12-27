18d743ed90776a11acdf22742baea083faaf029c
chiara1911 <chiaracesari89@gmail.com>
2023-12-27 17:43:51 +0100

    esempio array db template

---
+++ config/db_light.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+        'light' => [
+            [
+                'nome' => 'pino',
+                'prezzo' => '9,99',
+                'description' => 'ciaociaopippo',
+                'category' => 'luci',
+                'img' => 'ciaocia'
+
+            ],
+            [
+                'nome' => 'pino',
+                'prezzo' => '9,99',
+                'description' => 'ciaociaopippo',
+                'category' => 'luci',
+                'img' => 'ciaocia'
+
+            ],
+            [
+                'nome' => 'pino',
+                'prezzo' => '9,99',
+                'description' => 'ciaociaopippo',
+                'category' => 'luci',
+                'img' => 'ciaocia'
+
+            ]
+        ]
+]
+
+?>
---
+++ config/db_tree.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'tree' => [
+        [
+            'nome' => 'pino',
+            'prezzo' => '9,99',
+            'description' => 'ciaociaopippo',
+            'category' => 'green tree',
+            'img' => 'ciaocia'
+
+        ],
+        [
+            'nome' => 'pino',
+            'prezzo' => '9,99',
+            'description' => 'ciaociaopippo',
+            'category' => 'green tree',
+            'img' => 'ciaocia'
+
+        ],
+        [
+            'nome' => 'pino',
+            'prezzo' => '9,99',
+            'description' => 'ciaociaopippo',
+            'category' => 'green tree',
+            'img' => 'ciaocia'
+
+        ]
+        ],
+
+]
+?>

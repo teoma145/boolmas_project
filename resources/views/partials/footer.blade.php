5e6810874002a72199a1025a3d1061b7caa97aa6
chiara1911 <chiaracesari89@gmail.com>
2023-12-27 15:52:11 +0100

    add footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,35 @@
-<div>footer</div>
+<footer class="bg-dark text-light footer">
+    <div class="container">
+            <div class="row">
+                <div class="col-4 d-flex flex-column mt-4">
+                    <span class="text-uppercase"> boolxmas</span>
+                    <span> Via boolean 125</span>
+                    <span>P.Iva 0123456789</span>
+                </div>
+                <div class="col-4 d-flex flex-column mt-4">
+                    <h4 class="text-uppercase">orari di apertura</h4>
+                    <div class="d-flex flex-column">
+                        <span>Lunedì-Venerdì</span>
+                        <span> 09:30-12:30 / 14:00-19:00</span>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <span>Sabato-Domenica</span>
+                        <span> 09:30-12:30 </span>
+                    </div>
+
+                </div>
+                <div class="col-4 d-flex flex-column mt-4">
+                    <h4 class="text-uppercase">contatti</h4>
+                    <div class="d-flex flex-column">
+                        <span>Via Boolean, 125, Milano</span>
+                        <span>012-3456789</span>
+                        <span>info@boolean.bool</span>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </footer>
+
+

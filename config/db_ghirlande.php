de16ae5b9b2c827c88f8ce05ab98cd4463bbc33f
burillu <burillu90@hotmail.it>
2023-12-28 10:08:01 +0100

    created file with array ghirlande

---
+++ config/db_ghirlande.php
@@ -0,0 +1,93 @@
+<?php
+
+return [
+
+    'ghirlande' => [
+        [
+            'nome' => 'Babbo natale metallo ghirlanda rosso cm16x7x16',
+            'prezzo' => 11.70,
+            'description' => 'Ghirlanda con Babbo Natale in metallo con foglie verdi e bacche rosse cm 16x7x16',
+            'category' => 'ghirlande', // la categoria è sempre la stessa (in questo caso ghirlande)
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/601190.jpg' //
+
+        ],
+        [
+            'nome' => 'Corona fuoriporta decorata con luci',
+            'prezzo' => 42.90,
+            'description' => 'Corona fuoriporta, verde con decori rossi bianchi e verdi, diametro cm 60, con luci led bianco caldo a batteria.
+                Ghirlanda utilizzata made in Italy.',
+            'category' => 'ghirlande', // la categoria è sempre la stessa (in questo caso ghirlande)
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/corona-60-decorata.jpg' //
+
+        ],
+        [
+            'nome' => 'Corona fuoriporta innevata decorata con luci',
+            'prezzo' => 44.90,
+            'description' => 'Corona fuoriporta, innevata con decori oro e champagne, diametro cm 60, con luci led bianco caldo a batteria.
+                Ghirlanda utilizzata made in Italy.',
+            'category' => 'ghirlande', // la categoria è sempre la stessa (in questo caso ghirlande)
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/Corona-fuoriporta.jpg' //
+
+        ],
+        [
+            'nome' => 'FESTONE BACCO INNEVATO',
+            'prezzo' => 47.90,
+            'description' => 'DECORAZIONE NATALIZIA, FESTONE DI BACCHE ROSSE E PUNTE INNEVATE.',
+            'category' => 'ghirlande', // la categoria è sempre la stessa (in questo caso ghirlande)
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/DECORAZIONE-NATALIZIA-DI-BACCHE-ROSSE-.jpg' //
+
+        ],
+        [
+            'nome' => 'FIOCCO DA APPENDERE',
+            'prezzo' => 13.90,
+            'description' => 'FIOCCO ROSSO IMBOTTITO IN VELLUTO. DECORAZIONE NATALIZIA DA APPENDERE. DIMENSIONE: 40 X 29 CM',
+            'category' => 'ghirlande', // la categoria è sempre la stessa (in questo caso ghirlande)
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/FIOCCO-IN-VELLUTO.jpg' //
+
+        ],
+        [
+            'nome' => 'Fuoriporta decorato rosso e oro cm 35',
+            'prezzo' => 19.90,
+            'description' => 'Fuoriporta natalizio, decorato rosso ed oro, diametro 35 cm, ghirlanda made in italy.',
+            'category' => 'ghirlande', // la categoria è sempre la stessa (in questo caso ghirlande)
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/Fuoriporta-decorato-cm-35.jpg' //
+
+        ],
+        [
+            'nome' => 'Fuoriporta innevato diametro 60 cm',
+            'prezzo' => 66.90,
+            'description' => 'Fuoriporta natalizio, innevato con luci led a batteria. Diametro 60 cm.',
+            'category' => 'ghirlande', // la categoria è sempre la stessa (in questo caso ghirlande)
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/CORONA-DEC-NO-PIGNE.jpg' //
+
+        ],
+        [
+            'nome' => 'Fuoriporta natalizio innevato cm 60',
+            'prezzo' => 79.90,
+            'description' => 'Fuoriporta natalizio, innevato con luci led a batteria. Diametro 60 cm.',
+            'category' => 'ghirlande', // la categoria è sempre la stessa (in questo caso ghirlande)
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/CORONA-INNEVATA-CON-PIGNE.jpg' //
+
+        ],
+        [
+            'nome' => 'Ghirlanda fuoriporta innevata cm 60',
+            'prezzo' => 13.90,
+            'description' => 'Ghirlanda innevata tonda, diametro cm 35, made in Italy, ideale per decorare la porta o come centrotavola.',
+            'category' => 'ghirlande', // la categoria è sempre la stessa (in questo caso ghirlande)
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/Corona-60-innevata.jpg' //
+
+        ],
+        [
+            'nome' => 'Ghirlanda fuoriporta tonda verde cm 35',
+            'prezzo' => 6.90,
+            'description' => 'Ghirlanda verde tonda, diametro cm 60, made in Italy, ideale per decorare la porta o come centrotavola.',
+            'category' => 'ghirlande', // la categoria è sempre la stessa (in questo caso ghirlande)
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/CORONA-35-VDE.jpg' //
+
+        ],
+
+
+    ]
+]
+
+    ?>

9a3bf1518d58e155328d5ea77140ca35796f2366
burillu <burillu90@hotmail.it>
2023-12-28 18:24:15 +0100

    'prezzo' string => number

--- config/db_tree.php
+++ config/db_tree.php
@@ -4,70 +4,70 @@ return [
     'tree' => [
         [
             'nome' => 'Alberello verde altezza cm 65',
-            'prezzo' => '19,99',
+            'prezzo' => 19.99,
             'description' => 'Alberello di Natale di colore verde, altezza cm 65. Made in Italy, con base in legno.',
             'category' => 'green tree',
             'img' => 'https://emporiodelnatale.com/wp-content/uploads/albero-65-verde-BASSA.jpg'
         ],
         [
             'nome' => 'ALBERO CONO “DIAMONDS”',
-            'prezzo' => '21,90',
+            'prezzo' => 21.90,
             'description' => 'DECORAZIONE NATALIZIA, ALBERO A CONO IN METALLO CON LUCI A LED. LINEA “DIAMONDS”. DIMENSIONE: H60 X 16CM',
             'category' => 'green tree',
             'img' => 'https://emporiodelnatale.com/wp-content/uploads/ALBERO-CONO-H-60.jpg'
         ],
         [
             'nome' => 'ALBERO CONO “DIAMONDS” H 42 CM',
-            'prezzo' => '10,90',
+            'prezzo' => 10.90,
             'description' => 'DECORAZIONE NATALIZIA, ALBERO A CONO IN METALLO CON LUCI A LED. LINEA “DIAMONDS”. DIMENSIONE: H42 X 14CM',
             'category' => 'green tree',
             'img' => 'https://emporiodelnatale.com/wp-content/uploads/ALBERO-CONO-H-42.jpg'
         ],
         [
             'nome' => 'ALBERO CONO 80 CM',
-            'prezzo' => '29,90',
+            'prezzo' => 29.90,
             'description' => 'DECORAZIONE NATALIZIA, ALBERO A CONO IN METALLO CON LUCI A LED. LINEA “DIAMONDS”. DIMENSIONE: H80 X 18CM',
             'category' => 'green tree',
             'img' => 'https://emporiodelnatale.com/wp-content/uploads/ALBERO-CONO-80-CM.jpg'
         ],
         [
             'nome' => 'ALBERO CONO GLITTERATO 40 LED',
-            'prezzo' => '24,40',
+            'prezzo' => 24.40,
             'description' => 'ALBERO A CONO GLITTERATO. FORMATO DA 40 LUCI LED. DIMENSIONE DIAMETRO: 20 CM X H 80',
             'category' => 'green tree',
             'img' => 'https://emporiodelnatale.com/wp-content/uploads/ALBERO-CONO-GLITTER-40-LED.jpg'
         ],
         [
             'nome' => 'Albero di Natale Bormio, slim cm 210',
-            'prezzo' => '210,90',
+            'prezzo' => 210.90,
             'description' => 'Albero di natale verde slim, altezza cm 210, diametro cm 100, rami ad ombrello, base in metallo. Numero di rametti 2450.',
             'category' => 'green tree',
             'img' => 'https://emporiodelnatale.com/wp-content/uploads/210-slim-ecomm.jpg'
         ],
         [
             'nome' => 'Albero di Natale Bormio, slim cm 240',
-            'prezzo' => '259,90',
+            'prezzo' => 259.90,
             'description' => 'Albero di natale verde slim, altezza cm 240, diametro cm 110, rami ad ombrello, base in metallo. Numero di rametti 2996.',
             'category' => 'green tree',
             'img' => 'https://emporiodelnatale.com/wp-content/uploads/240-slim-ecomm.jpg'
         ],
         [
             'nome' => 'Albero di Natale Campiglio cm 210',
-            'prezzo' => '279,90',
+            'prezzo' => 279.90,
             'description' => 'Albero di natale verde, altezza cm 210, diametro cm 130, rami ad ombrello, base in metallo. Numero di rametti 2382.',
             'category' => 'green tree',
             'img' => 'https://emporiodelnatale.com/wp-content/uploads/pe-grande-bassa-quali.jpg'
         ],
         [
             'nome' => 'Albero di Natale Canazei cm 120',
-            'prezzo' => '99,90',
+            'prezzo' => 99.90,
             'description' => 'Albero di natale artificiale, modello Canazei. Albero con costruzione ad ombrello, con 746 rametti in pvc e PE, con aggiunta di pigne naturali. Altezza cm 1520, diametro di base cm 84. Base in metallo.',
             'category' => 'green tree',
             'img' => 'https://emporiodelnatale.com/wp-content/uploads/Albero-di-natale-Canazei.jpg'
         ],
         [
             'nome' => 'Albero di Natale Canazei cm 150',
-            'prezzo' => '179,90',
+            'prezzo' => 179.90,
             'description' => 'Albero con costruzione ad ombrello, con 1274 rametti in pvc e PE, con aggiunta di pigne naturali. Altezza cm 150, diametro di base cm 107. Base in metallo.',
             'category' => 'green tree',
             'img' => 'https://emporiodelnatale.com/wp-content/uploads/Albero-di-natale-Canazei.jpg'
--- config/db_tree_decoration.php
+++ config/db_tree_decoration.php
@@ -2,88 +2,88 @@
 
 return [
 
-        'decoro_albero' => [
-            [
-                'nome' => 'Alberello biscotto 10 cm',
-                'prezzo' => '3,70',
-                'description' => 'Alberello biscotto da appendere, dimensione cm 10.Realizzato in argilla morbida.',
-                'category' => 'decorazioni albero',
-                'img' => 'https://emporiodelnatale.com/wp-content/uploads/2022-67771-A.jpg' ,
+    'decoro_albero' => [
+        [
+            'nome' => 'Alberello biscotto 10 cm',
+            'prezzo' => 3.70,
+            'description' => 'Alberello biscotto da appendere, dimensione cm 10.Realizzato in argilla morbida.',
+            'category' => 'decorazioni albero',
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/2022-67771-A.jpg',
 
-            ],
-            [
-                'nome' => 'Angioletto bianco 13 cm',
-                'prezzo' => '3,20',
-                'description' => 'Angioletto da appendere bianco, altezza cm 13',
-                'category' => 'decorazioni albero',
-                'img' => 'https://emporiodelnatale.com/wp-content/uploads/Bambolina-angelo-cm-13-bianca.jpg'
+        ],
+        [
+            'nome' => 'Angioletto bianco 13 cm',
+            'prezzo' => 3.20,
+            'description' => 'Angioletto da appendere bianco, altezza cm 13',
+            'category' => 'decorazioni albero',
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/Bambolina-angelo-cm-13-bianca.jpg'
 
-            ],
-            [
-                'nome' => 'Appenderia bimbo color panna cm 14',
-                'prezzo' => '3,30',
-                'description' => 'Soggetto da appendere all’albero, bimbo color panna, altezza cm 14',
-                'category' => 'decorazioni albero',
-                'img' => 'https://emporiodelnatale.com/wp-content/uploads/appenderla-panna-cm-14.jpg'
+        ],
+        [
+            'nome' => 'Appenderia bimbo color panna cm 14',
+            'prezzo' => 3.30,
+            'description' => 'Soggetto da appendere all’albero, bimbo color panna, altezza cm 14',
+            'category' => 'decorazioni albero',
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/appenderla-panna-cm-14.jpg'
 
-            ],
-            [
-                'nome' => 'Babbo natale con stella resina bianco cm 6x11,5',
-                'prezzo' => '€6,90',
-                'description' => 'Babbo Natale con stella oro in mano, in resina cm6x11,5',
-                'category' => 'decorazioni albero',
-                'img' => 'https://emporiodelnatale.com/wp-content/uploads/a.png'
+        ],
+        [
+            'nome' => 'Babbo natale con stella resina bianco cm 6x11,5',
+            'prezzo' => 6.90,
+            'description' => 'Babbo Natale con stella oro in mano, in resina cm6x11,5',
+            'category' => 'decorazioni albero',
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/a.png'
 
-            ],
-            [
-                'nome' => 'Bastoncino plastica verde rosso cm 4,4x1,2x18,7',
-                'prezzo' => '€3,95',
-                'description' => 'astoncino in plastica a forma di Candy Canes, da appendere cm4,4x1,2x18,7',
-                'category' => 'decorazioni albero',
-                'img' => 'https://emporiodelnatale.com/wp-content/uploads/596511.jpg'
+        ],
+        [
+            'nome' => 'Bastoncino plastica verde rosso cm 4,4x1,2x18,7',
+            'prezzo' => 3.95,
+            'description' => 'astoncino in plastica a forma di Candy Canes, da appendere cm4,4x1,2x18,7',
+            'category' => 'decorazioni albero',
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/596511.jpg'
 
-            ],
-            [
-                'nome' => 'Casetta legno con babbo natale cm10x1,2x28,6',
-                'prezzo' => '€2,80',
-                'description' => 'Casetta in legno naturale con Babbo Natale e Albero cm10x1,2x28,6',
-                'category' => 'decorazioni albero',
-                'img' => 'https://emporiodelnatale.com/wp-content/uploads/602455.jpg'
+        ],
+        [
+            'nome' => 'Casetta legno con babbo natale cm10x1,2x28,6',
+            'prezzo' => 2.80,
+            'description' => 'Casetta in legno naturale con Babbo Natale e Albero cm10x1,2x28,6',
+            'category' => 'decorazioni albero',
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/602455.jpg'
 
-            ],
-            [
-                'nome' => 'Catena Luminosa, 2,40m, 48 led, multicolor, batteria',
-                'prezzo' => '€7,90',
-                'description' => 'Catena Luminosa di 2,40m con 48 led di luce multicolor(distanza 5cm), a batteria.',
-                'category' => 'decorazioni albero',
-                'img' => 'https://emporiodelnatale.com/wp-content/uploads/Cattura-8.jpg'
+        ],
+        [
+            'nome' => 'Catena Luminosa, 2,40m, 48 led, multicolor, batteria',
+            'prezzo' => 7.90,
+            'description' => 'Catena Luminosa di 2,40m con 48 led di luce multicolor(distanza 5cm), a batteria.',
+            'category' => 'decorazioni albero',
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/Cattura-8.jpg'
 
-            ],
-            [
-                'nome' => 'Ciambella da appendere 8 cm',
-                'prezzo' => '€3,20',
-                'description' => 'Ciambella decorativa da appendere, realizzata in argilla morbida.Diametro centimetri 8.Ideale per decorare alberi e ghirlande di Natale.',
-                'category' => 'decorazioni albero',
-                'img' => 'https://emporiodelnatale.com/wp-content/uploads/2022-67962-C.jpg'
+        ],
+        [
+            'nome' => 'Ciambella da appendere 8 cm',
+            'prezzo' => 3.20,
+            'description' => 'Ciambella decorativa da appendere, realizzata in argilla morbida.Diametro centimetri 8.Ideale per decorare alberi e ghirlande di Natale.',
+            'category' => 'decorazioni albero',
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/2022-67962-C.jpg'
 
-            ],
-            [
-                'nome' => 'Cuore in tessuto bianco cm 12',
-                'prezzo' => '€2,60',
-                'description' => 'Cuore in tessuto bianco, dimensione cm 12',
-                'category' => 'decorazioni albero',
-                'img' => 'https://emporiodelnatale.com/wp-content/uploads/Cuore-in-tessuto-bianco.jpg'
+        ],
+        [
+            'nome' => 'Cuore in tessuto bianco cm 12',
+            'prezzo' => 2.60,
+            'description' => 'Cuore in tessuto bianco, dimensione cm 12',
+            'category' => 'decorazioni albero',
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/Cuore-in-tessuto-bianco.jpg'
 
-            ],
-            [
-                'nome' => 'Decorazione elfo natalizio in argilla',
-                'prezzo' => '€1,90',
-                'description' => 'Decorazione in Clay, elfo natalizio da appendere, cm 11',
-                'category' => 'decorazioni albero',
-                'img' => 'https://emporiodelnatale.com/wp-content/uploads/Elfo-natalizio-in-clay.jpg'
+        ],
+        [
+            'nome' => 'Decorazione elfo natalizio in argilla',
+            'prezzo' => 1.90,
+            'description' => 'Decorazione in Clay, elfo natalizio da appendere, cm 11',
+            'category' => 'decorazioni albero',
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/Elfo-natalizio-in-clay.jpg'
 
-            ],
-        ]
+        ],
+    ]
 ]
 
-?>
+    ?>

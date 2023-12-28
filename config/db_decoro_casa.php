de0c53172bc38b65bc8ef74e7a1ea3e630bdb479
burillu <burillu90@hotmail.it>
2023-12-28 10:30:12 +0100

    created file with array decoro_casa

---
+++ config/db_decoro_casa.php
@@ -0,0 +1,91 @@
+<?php
+
+return [
+
+    'decoro_casa' => [
+        [
+            'nome' => 'Albero in metallo porta tea-light',
+            'prezzo' => 3.10,
+            'description' => 'Albero in metallo dorato porta tea-ligh',
+            'category' => 'decorazioni casa', // la categoria è sempre la stessa (in questo caso decorazioni casa)
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/albero-in-metallo-porta-tea-light.jpg' //
+
+        ],
+        [
+            'nome' => 'Coroncina decorativa oro cm 10',
+            'prezzo' => 1.80,
+            'description' => 'Coroncina decorativa oro, diametro cm 10.
+                Può essere utilizzata sia come decorazione per albero che come ornacandela.',
+            'category' => 'decorazioni casa', // la categoria è sempre la stessa (in questo caso decorazioni casa)
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/coroncina-2021.jpg' //
+
+        ],
+        [
+            'nome' => 'Lanterna a forma di casetta',
+            'prezzo' => 8.90,
+            'description' => 'Lanterna a Forma di casetta color crema. Dimensione 9,3x10h15.',
+            'category' => 'decorazioni casa', // la categoria è sempre la stessa (in questo caso decorazioni casa)
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/240459.jpg' //
+
+        ],
+        [
+            'nome' => 'Orna candela bacche cm 19',
+            'prezzo' => 4.60,
+            'description' => 'Orna candela con bacche. Diametro interno cm 8, diametro esterno cm 19.',
+            'category' => 'decorazioni casa', // la categoria è sempre la stessa (in questo caso decorazioni casa)
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/ornacandela-bacche.jpg' //
+
+        ],
+        [
+            'nome' => 'Ornacandele con mele cm 14',
+            'prezzo' => 3.00,
+            'description' => 'Orna candele con mele e neve. Diametro interno cm 4, diametro esterno cm 14.',
+            'category' => 'decorazioni casa', // la categoria è sempre la stessa (in questo caso decorazioni casa)
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/ornacndela-neve.jpg' //
+
+        ],
+        [
+            'nome' => 'Portacandela a specchio cm 10 con perle',
+            'prezzo' => 1.60,
+            'description' => 'Portacandela a specchio, diametro 10 cm, con perle sul bordo',
+            'category' => 'decorazioni casa', // la categoria è sempre la stessa (in questo caso decorazioni casa)
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/Portacandela-specchio-cm-10-perle.jpg' //
+
+        ],
+        [
+            'nome' => 'Portacandele con bacche 4 fuochi',
+            'prezzo' => 37.90,
+            'description' => 'Portacandele con bacche, 4 fuochi, diametro 46 cm h 14',
+            'category' => 'decorazioni casa', // la categoria è sempre la stessa (in questo caso decorazioni casa)
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/Portacandela-bacche-4-fuochi.jpg' //
+
+        ],
+        [
+            'nome' => 'Portacandele con pigne e bacche 3 fuochi',
+            'prezzo' => 33.90,
+            'description' => 'Portacandela con pigne e bacche, 3 fuochi , cm 70 x 30 x h 22',
+            'category' => 'decorazioni casa', // la categoria è sempre la stessa (in questo caso decorazioni casa)
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/Portacandela-con-bacche-3-fuochi.jpg' //
+
+        ],
+        [
+            'nome' => 'Portacandele con vischio 3 fuochi',
+            'prezzo' => 39.90,
+            'description' => 'Portacandele con vischio, 3 fuochi , cm 70 x 30 x h 22',
+            'category' => 'decorazioni casa', // la categoria è sempre la stessa (in questo caso decorazioni casa)
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/Portacandela-vischio-3-fuochi.jpg' //
+
+        ],
+        [
+            'nome' => 'Stella in metallo porta tea-light',
+            'prezzo' => 3.10,
+            'description' => 'Stella in metallo dorato porta tea-light',
+            'category' => 'decorazioni casa', // la categoria è sempre la stessa (in questo caso decorazioni casa)
+            'img' => 'https://emporiodelnatale.com/wp-content/uploads/stella-in-metallo-porta-tealight.jpg' //
+
+        ]
+
+    ]
+]
+
+    ?>

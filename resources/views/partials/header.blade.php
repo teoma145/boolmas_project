37e8635804236e37b2d7a8ad023e78bba414a4f1
teoma145 <matteodark145@yahoo.it>
2023-12-29 17:40:08 +0100

    navbar-fixed

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -11,4 +11,30 @@
             <span><a href=""><i class="fa-solid fa-cart-shopping" style="color: #000000;"></i></a></span>
         </div>
     </div>
+    <hr>
+    <div class="bg-white">
+        <div class="container">
+            <ul class="d-flex list-unstyled fs-4 justify-content-between ">
+                <li><a href="">Decorazioni casa</a></li>
+                <li><a href="">Ghirlande</a></li>
+                <li><a href="">Luci</a></li>
+                <li><a href="">Decorazioni alberi</a></li>
+                <li><a href="">Alberi di natale</a></li>
+            </ul>
+        </div>
+    </div>
 </div>
+<script>
+    window.onscroll = function() {
+        var navbar = document.querySelector(".bg-white");
+        var currentScrollPos = window.pageYOffset;
+
+        if (currentScrollPos > 200) {
+
+            navbar.classList.add("fixed-top");
+        }
+        else {
+            navbar.classList.remove("fixed-top");
+        }
+    }
+</script>

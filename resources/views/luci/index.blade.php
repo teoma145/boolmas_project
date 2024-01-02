f2c37bc8690519fafa165d78a66fc06a7e5eb34f
chiara1911 <chiaracesari89@gmail.com>
2024-01-02 13:29:08 +0100

    add light page with card

---
+++ resources/views/luci/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('title', 'Luci')
+
+@section('content')
+    <main>
+
+        <div class="container">
+
+            <div class="row">
+                @foreach ($products as $product)
+                    <div class="col-4">
+                        <div class="card">
+                            <img src="{{ $product->img }}" class="card-img-top" alt="{{ $product->nome }}">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $product->nome }}</h5>
+                                <p class="card-text">{{ $product->prezzo }}</p>
+                                <a href="#" class="btn btn-primary">Aggiungi al carrello</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+
+    </main>
+
+@endsection

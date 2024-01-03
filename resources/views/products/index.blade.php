81dbcb6ddecf968007779030c3d99bf4e4fea319
Riccardo Potalivo <riccardopotalivo@gmail.com>
2024-01-03 15:36:59 +0100

    view product index create

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('title', $title)
+
+@section('content')
+    <main>
+
+        <div class="container">
+
+            <div class="row">
+                @foreach ($products as $product)
+                    <div class="col-4">
+                        <div class="card">
+                            <a href="{{ route('prodotto.show', $product->id) }}"><img src="{{ $product->img }}"
+                                    class="card-img-top" alt="{{ $product->nome }}"></a>
+
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $product->nome }} </h5>
+                                <p class="card-text">{{ $product->prezzo }} euro</p>
+                                <a href="{{ route('prodotto.show', $product->id) }}" class="btn btn-primary">Aggiungi al
+                                    carrello</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+
+    </main>
+
+@endsection

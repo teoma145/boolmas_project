b4d1cd74aeb826ba14e3e01128d42d3bdeaa85bd
burillu <burillu90@hotmail.it>
2024-01-03 11:14:52 +0100

    improve 'prezzo' with dot

--- config/db_light.php
+++ config/db_light.php
@@ -5,7 +5,7 @@ return [
     'light' => [
         [
             'nome' => '1.200 led bianco caldo uso interno ed esterno, 24 metri',
-            'prezzo' => 49, 90,
+            'prezzo' => 49.90,
             'description' => 'Collana luminosa 1.200 Led, bianco caldo con 8 giochi di luce e funzione memory.
                 Adatte ad un uso interno od esterno, distanza tra ogni led 2 cm, lunghezza totale delle luci 24 metri.',
             'category' => 'luci',
@@ -14,7 +14,7 @@ return [
         ],
         [
             'nome' => '1.500 led bianco caldo uso interno ed esterno, 30 metri',
-            'prezzo' => 59, 90,
+            'prezzo' => 59.90,
             'description' => 'Collana luminosa 1.500 Led, bianco caldo con 8 giochi di luce e funzione memory. Adatte ad un uso interno od esterno, distanza tra ogni led 2 cm, lunghezza totale delle luci 30 metri.',
             'category' => 'luci',
             'img' => 'https://emporiodelnatale.com/wp-content/uploads/1500-led-bianco-caldo.jpg'
@@ -22,7 +22,7 @@ return [
         ],
         [
             'nome' => '300 led bianco caldo uso interno ed esterno, 21 metri',
-            'prezzo' => 227, 90,
+            'prezzo' => 227.90,
             'description' => 'Collana luminosa 300 Led, bianco caldo con 8 giochi di luce e funzione memory.
                 Adatte ad un uso interno od esterno, distanza tra ogni led 7 cm, lunghezza totale delle luci 21 metri.',
             'category' => 'luci',
@@ -31,7 +31,7 @@ return [
         ],
         [
             'nome' => '480 led bianco caldo uso interno ed esterno, 24 metri',
-            'prezzo' => 35, 90,
+            'prezzo' => 35.90,
             'description' => 'Collana luminosa 480 Led, bianco caldo con 8 giochi di luce e funzione memory.
                 Adatte ad un uso interno od esterno, distanza tra ogni led 5 cm, lunghezza totale delle luci 24 metri.',
             'category' => 'luci',
@@ -40,7 +40,7 @@ return [
         ],
         [
             'nome' => '800 led bianco caldo uso interno ed esterno, 16 metri',
-            'prezzo' => 39, 90,
+            'prezzo' => 39.90,
             'description' => 'Collana luminosa 800 Led, bianco caldo con 8 giochi di luce e funzione memory.
                 Adatte ad un uso interno od esterno, distanza tra ogni led 2 cm, lunghezza totale delle luci 16 metri.',
             'category' => 'luci',
@@ -49,7 +49,7 @@ return [
         ],
         [
             'nome' => 'Albero a cono con 300 led bianco caldo, cm 80',
-            'prezzo' => 49, 90,
+            'prezzo' => 49.90,
             'description' => 'Albero a cono in metallo, con 300 nano led colore bianco caldo.
                 Dimensione cm 80, diametro di base 20 cm.',
             'category' => 'luci',
@@ -58,7 +58,7 @@ return [
         ],
         [
             'nome' => 'Albero a cono con 500 led bianco caldo cm 120',
-            'prezzo' => 128, 90,
+            'prezzo' => 128.90,
             'description' => 'Albero a cono in metallo, con 500 nano led colore bianco caldo.
                 Dimensione cm 120, diametro di base 28 cm.',
             'category' => 'luci',
@@ -67,7 +67,7 @@ return [
         ],
         [
             'nome' => 'Albero cono "diamonds"',
-            'prezzo' => 21, 90,
+            'prezzo' => 21.90,
             'description' => 'decorazione natalizia, albero a cono in metallo con luci a led. Linea "Diamonds. Dimensione : H60 x 16 cm',
             'category' => 'luci',
             'img' => 'https://emporiodelnatale.com/wp-content/uploads/ALBERO-CONO-H-60.jpg'
@@ -76,7 +76,7 @@ return [
         ],
         [
             'nome' => 'Albero a cono "diamonds" h 42 cm',
-            'prezzo' => 10, 90,
+            'prezzo' => 10.90,
             'description' => 'Decorazione natalizia, albero a cono in metallo con luci a led. Linea "diamonds". Dimensione : H 42 x 14cm',
             'category' => 'luci',
             'img' => 'https://emporiodelnatale.com/wp-content/uploads/ALBERO-CONO-H-42.jpg'
@@ -84,7 +84,7 @@ return [
         ],
         [
             'nome' => 'Albero a cono 80 cm',
-            'prezzo' => 29, 90,
+            'prezzo' => 29.90,
             'description' => 'Decorazione natalizia, albero a cono in metallo con luci a led. Linea "diamonds". Dimensione : H 80 x 18cm',
             'category' => 'luci',
             'img' => 'https://emporiodelnatale.com/wp-content/uploads/ALBERO-CONO-80-CM.jpg'

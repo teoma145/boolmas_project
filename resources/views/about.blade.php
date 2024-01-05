03016c87fd05adcf58250f9a925633630f07deab
burillu <burillu90@hotmail.it>
2024-01-06 00:32:05 +0100

    add img in about improve layout and style

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,15 +5,28 @@
 @section('content')
     <main>
 
-        <div class="container">
-            <h2 class="text-center my-5">Chi siamo</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos magni voluptates at laborum ex
-                assumenda aut, blanditiis modi adipisci impedit? Veniam voluptates repellendus corporis recusandae in nam
-                animi sequi exercitationem? Sint, error cupiditate, ad dolor natus quas pariatur quidem exercitationem minus
-                totam corrupti asperiores provident? Minus dolor illum mollitia magnam et saepe quisquam nisi delectus error
-                esse. Ab dignissimos, quisquam magni, a earum quas accusamus, corrupti sed culpa modi consequatur.
-                Consequuntur fuga dolorem magnam pariatur praesentium rem eum eaque distinctio, odio quidem excepturi quod?
-                Culpa nulla enim molestias quidem laudantium.</p>
+        <div class="container-fluid p-0">
+            <div class="row">
+                <div class="col-12 col-md-6">
+                    <img src="https://nypost.com/wp-content/uploads/sites/2/2020/12/fire-christmas.jpg?quality=75&strip=all"
+                        alt="christmas living room">
+                </div>
+                <div class="col-12 col-md-6">
+                    <h2 class="text-center my-5">Chi siamo</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos magni voluptates at laborum ex
+                        assumenda aut, blanditiis modi adipisci impedit? Veniam voluptates repellendus corporis recusandae
+                        in nam
+                        animi sequi exercitationem? Sint, error cupiditate, ad dolor natus quas pariatur quidem
+                        exercitationem minus
+                        totam corrupti asperiores provident? Minus dolor illum mollitia magnam et saepe quisquam nisi
+                        delectus error
+                        esse. Ab dignissimos, quisquam magni, a earum quas accusamus, corrupti sed culpa modi consequatur.
+                        Consequuntur fuga dolorem magnam pariatur praesentium rem eum eaque distinctio, odio quidem
+                        excepturi quod?
+                        Culpa nulla enim molestias quidem laudantium.</p>
+                </div>
+            </div>
+
 
 
         </div>

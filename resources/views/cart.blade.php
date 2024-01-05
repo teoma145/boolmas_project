ff030d4d8bdc0a8ca656760989b0e98ea679abec
burillu <burillu90@hotmail.it>
2024-01-06 00:13:02 +0100

    add container for content, translate th text in italian

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -3,38 +3,41 @@
 @section('title', 'cart')
 
 @section('content')
-    <table id="cart" class="table table-bordered">
-        <thead>
-            <tr>
-                <th>Product</th>
-                <th>Price</th>
-                <th>Total</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @if(session('cart'))
-                @foreach (session('cart') as $id => $details)
-                    <tr rowId='{{$id}}'>
-                        <td data-th='Product'>
-                            <div class="row">
-                                <div class="col-sm-9">
-                                    <h4 class="nomargin">
-                                        {{ $details['name'] }}
-                                    </h4>
+    <div class="container">
+        <table id="cart" class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Prodotti</th>
+                    <th>Prezzi</th>
+                    <th>Totale</th>
+                    <th>Azioni</th>
+                </tr>
+            </thead>
+            <tbody>
+                @if (session('cart'))
+                    @foreach (session('cart') as $id => $details)
+                        <tr rowId='{{ $id }}'>
+                            <td data-th='Product'>
+                                <div class="row">
+                                    <div class="col-sm-9">
+                                        <h4 class="nomargin">
+                                            {{ $details['name'] }}
+                                        </h4>
+                                    </div>
                                 </div>
-                            </div>
-                        </td>
-                        <td data-th="Price">${{ $details['price'] }}</td>
-                        <td data-th="Total" class="text-center">${{ $details['price'] * $details['quantity'] }}</td>
-                        <td class="actions">
-                            <a href="" class="btn btn-outline-danger btn-sm delete-product"><i
-                                    class="fa fa-trash"></i></a>
-                        </td>
-                    </tr>
-                @endforeach
-            @endif
-        </tbody>
-    </table>
-    <a href="{{ route('clear.cart') }}" class="btn btn-danger">Svuota Carrello</a>
+                            </td>
+                            <td data-th="Price">&euro;{{ $details['price'] }}</td>
+                            <td data-th="Total" class="text-center">&euro;{{ $details['price'] * $details['quantity'] }}</td>
+                            <td class="actions">
+                                <a href="" class="btn btn-outline-danger btn-sm delete-product"><i
+                                        class="fa fa-trash"></i></a>
+                            </td>
+                        </tr>
+                    @endforeach
+                @endif
+            </tbody>
+        </table>
+        <a href="{{ route('clear.cart') }}" class="btn btn-danger">Svuota Carrello</a>
+    </div>
+
 @endsection
